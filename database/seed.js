8e696aca6b70ebbdf8ef9ff8f77c38dc24b23cce
ahchung23 <hahneol@gmail.com>
2019-03-31 00:41:48 -0400

    delete mysql cli to setup in config.yml
    try seeding database using db.query

--- database/seed.js
+++ database/seed.js
@@ -24,3 +24,57 @@ db.queryAsync('use books')
       .catch((err) => console.log('err seeding db', err));
   });
 
+var createTables = (db) => {
+
+  return db.queryAsync(`
+    CREATE TABLE IF NOT EXISTS details (
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      type VARCHAR(20),
+      pagenum INT,
+      publisher VARCHAR(100),
+      firstPubDate VARCHAR(30),
+      originalPubDate VARCHAR(30),
+      title VARCHAR(100),
+      isbn10 VARCHAR(20),
+      isbn13 VARCHAR(20),
+      language VARCHAR(20)
+      );`
+  )
+    .then(() => {
+      return db.queryAsync(`
+        CREATE TABLE IF NOT EXISTS characters (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(100),
+          bookId INT
+        );`
+      );
+    })
+    .then(() => {
+      return db.queryAsync(`
+        CREATE TABLE IF NOT EXISTS awards (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(100),
+          year INT,
+          bookId INT
+        );`
+      );
+    })
+    .then(() => {
+      return db.queryAsync(`
+        CREATE TABLE IF NOT EXISTS editions (
+          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          isbn10 VARCHAR(20),
+          isbn13 VARCHAR(20),
+          type VARCHAR(20),
+          publisher VARCHAR(100),
+          originalPubDate VARCHAR(30),
+          coverurl VARCHAR(250),
+          bookId INT
+        );`
+      );
+    })
+    .error((err) => {
+      console.log('error making tables', err);
+    });
+
+};
