98f7a8b692e5d4aefcae313a0479935ed878896d
ahchung23 <hahneol@gmail.com>
2019-04-04 16:26:15 -0400

    get details  data from server/db with axios and setState
    sends get request to endpoint '/books/:id/details'

--- client/src/components/details.jsx
+++ client/src/components/details.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 
-const Details = (props) => {
-  console.log(props.match.params.id)
-  return (
-    <div>
-      WHAT?!?!?!?
-    </div>
-  )
+class Details extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggle: false,
+      id: (this.props.match.params.id),
+      details: null
+    }
+  }
+
+  componentDidMount() {
+    if (this.state.details === null) {
+      this.getMainDetails();
+    }
+  }
+
+  getMainDetails() {
+    const { id } = this.state;
+    axios.get(`/books/${id}/details`)
+      .then((res) => {
+        this.setState({
+          details: res.data
+        })
+      })
+      .catch((err) => console.log('error get details', err));
+  }
+
+
+  render() {
+    console.log(this.state);
+    const {type, pagenum, originalPubDate, firstPubDate, publisher} = this.state.details
+
+    return (
+      <div>
+        {`${type}, ${pagenum} pages\n
+        Published ${originalPubDate} by ${publisher} (first published ${firstPubDate})`}
+      </div>
+    )
+  }
 }
 
 
