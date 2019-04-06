1fbecb32e77945a84d1ebffaeac4b64bc92f1995
ahchung23 <hahneol@gmail.com>
2019-04-06 10:12:46 -0400

    edit tooltip content to show basic info and want to read button

--- client/src/components/editions_components/tooltip.jsx
+++ client/src/components/editions_components/tooltip.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import { GreenButton, GreyItem } from '../header.jsx';
 
 const Wrapper = styled.div`
   padding: 0px;
   margin: 0px 0px 5px;
   min-width: 300px;
+  max-width: 330px;
   height: auto;
   clear: both;
   z-index: 10;
@@ -16,7 +18,7 @@ const Wrapper = styled.div`
   overflow: visible;
 
   font-family: "Lato", "Helvetica Neue", "Helvetica", sans-serif;
-  font-size: 11px;
+  font-size: 12px;
   color: #333333;
 `;
 
@@ -53,19 +55,65 @@ const Book = styled.div`
   width: 100%;
 `;
 
+const StatusWrapper = styled.div`
+  display: flex;
+  border-width: 10px;
+  border-radius: 3px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-right: 8px;
+  padding: 5px 10px;
+  width: 110px;
+  background-color: #409D69;
+  border-color: #dddddd;
+
+  text-align: left;
+  font-size: 13px;
+  font-weight: bold;
+  font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: white;
+
+`;
+
+export const RightButton = styled.span`
+  border-left: 1px solid #38883d;
+`;
+
+const Arrow = styled.div`
+  width: 0;
+  height: 0;
+  border-left: 3px solid transparent;
+  border-right: 3px solid transparent;
+  border-top: 3px solid white;
+  left: 11px
+  top: 7px
+  position: relative;
+`;
+
 const Tooltip = (props) => {
   console.log('what is tooltip props? \n', props);
+  const {
+    isbn10, isbn13, originalPubDate, publisher, title, type,
+  } = props;
 
   return (
     <Wrapper>
       <BorderFrame>
         <BorderCenter>
           <Book>
-            TOOLTIP
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+            <div>{`${title} (${type})`}</div>
+            <div>{`isbn: ${isbn10}`}</div>
+            <div>{`isbn13: ${isbn13}`}</div>
+            <div>{`format: ${type}`}</div>
+            <div>{`Published ${originalPubDate} by ${publisher}`}</div>
+            <GreenButton>Enlarge cover</GreenButton>
+            <StatusWrapper>
+              {/* <div style={{ color: '#63ce92' }}></div> */}
+              <span title="status" style={{ paddingRight: '10px' }}>Want to Read</span>
+              <RightButton><Arrow /></RightButton>
+
+            </StatusWrapper>
+
           </Book>
         </BorderCenter>
       </BorderFrame>
@@ -75,3 +123,13 @@ const Tooltip = (props) => {
 };
 
 export default Tooltip;
+
+{/* <Wrapper>
+<DropDown>
+  <div style={{ color: '#63ce92' }}><DoneIcon /></div>
+  <span title="Read">Read</span>
+</DropDown>
+  <RightButton>
+    <ShelfButton></ShelfButton>
+  </RightButton>
+</Wrapper> */}
