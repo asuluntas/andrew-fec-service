38f977a84432a3a3f00c7bd31d6a2e71f1c03d43
ahchung23 <hahneol@gmail.com>
2019-04-07 08:26:30 -0400

    fix server side tests to be independent of db.
    mock db functions and sample data

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,7 +4,16 @@ const app = require('../app');
 const db = require('../../database/index');
 
 describe('Test endpoint GET /books/:id/details', () => {
+  let mock;
+  afterEach(() => {
+    if (mock) mock.mockRestore();
+  });
+
   test('It should give correct status code response to the GET method', (done) => {
+    mock = jest.spyOn(db, 'getDetails');
+    const data = [[{ id: 1 }]];
+    mock.mockImplementation(() => Promise.resolve(data));
+
     request(app).get(`/books/${randomInt}}/details`).then((response) => {
       expect(response.statusCode).toBe(200);
       done();
@@ -13,6 +22,11 @@ describe('Test endpoint GET /books/:id/details', () => {
 
   test('It should give back body corresponding to right id', (done) => {
     const num = randomInt;
+
+    mock = jest.spyOn(db, 'getDetails');
+    const data = [[{ id: num }]];
+    mock.mockImplementation(() => Promise.resolve(data));
+
     request(app).get(`/books/${num}}/details`).then((response) => {
       expect(response.body.id).toBe(num);
       done();
@@ -20,7 +34,24 @@ describe('Test endpoint GET /books/:id/details', () => {
   });
 
   test('It should have response.body object with correct properties if object exits', (done) => {
-    request(app).get(`/books/${randomInt}}/details`).then((response) => {
+    const num = randomInt;
+    const data = [[
+      {
+        id: num,
+        type: 'type',
+        pagenum: 10,
+        firstPubDate: 'firstPubDate',
+        originalPubDate: 'orgPubDate',
+        title: 'title',
+        isbn10: '123',
+        isbn13: '12345',
+        language: 'language',
+      },
+    ]];
+    mock = jest.spyOn(db, 'getDetails');
+    mock.mockImplementation(() => Promise.resolve(data));
+
+    request(app).get(`/books/${num}}/details`).then((response) => {
       expect(response.body).toEqual(expect.objectContaining({
         id: expect.any(Number),
         type: expect.any(String),
@@ -37,6 +68,9 @@ describe('Test endpoint GET /books/:id/details', () => {
   });
 
   test('It should give error response when id does not exist', (done) => {
+    mock = jest.spyOn(db, 'getDetails');
+    mock.mockImplementation(() => Promise.resolve([[]]));
+
     request(app).get(`/books/${randomInt + 100}}/details`).then((response) => {
       expect(response.statusCode).toBe(404);
       done();
@@ -51,16 +85,32 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
     db.end(() => { done(); });
   });
 
+  let mock;
+
+  afterEach(() => {
+    if (mock) mock.mockRestore();
+  });
+
   describe('Test response for endpoint /books/:id/details/characters', () => {
     test('It should give correct status code response to GET characters table', (done) => {
-      request(app).get(`/books/${randomInt}}/details/characters`).then((response) => {
+      const num = randomInt;
+      const data = [[{ id: 1, name: 'name', bookId: num }]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
+      request(app).get(`/books/${num}}/details/characters`).then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
     test('It should give error response when id does not exist', (done) => {
-      request(app).get(`/books/${randomInt + 100}}/details/characters`).then((response) => {
+      const num = randomInt;
+      const data = [[]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
+      request(app).get(`/books/${num + 100}}/details/characters`).then((response) => {
         expect(response.statusCode).toBe(404);
         done();
       });
@@ -68,6 +118,10 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
     test('It should give back body obj corresponding to right id with correct format', (done) => {
       const num = randomInt;
+      const data = [[{ id: 1, name: 'name', bookId: num }]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
       request(app).get(`/books/${num}}/details/characters`).then((response) => {
         response.body.forEach((char) => {
           expect(char.bookId).toBe(num);
@@ -82,13 +136,30 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
   describe('Test response for endpoint /books/:id/details/awards', () => {
     test('It should give correct status code response to GET awards table', (done) => {
-      request(app).get(`/books/${randomInt}}/details/awards`).then((response) => {
+      const num = randomInt;
+      const data = [[
+        {
+          id: 670,
+          name: 'National Book Award',
+          year: 2014,
+          bookId: num,
+        },
+      ]];
+
+
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
+      request(app).get(`/books/${num}}/details/awards`).then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
     test('It should give error response when id does not exist', (done) => {
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve([[]]));
+
       request(app).get(`/books/${randomInt + 100}}/details/awards`).then((response) => {
         expect(response.statusCode).toBe(404);
         done();
@@ -97,6 +168,17 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
     test('It should give back body obj corresponding to right id with correct format', (done) => {
       const num = randomInt;
+      const data = [[
+        {
+          id: 670,
+          name: 'National Book Award',
+          year: 2014,
+          bookId: num,
+        },
+      ]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
       request(app).get(`/books/${num}}/details/awards`).then((response) => {
         response.body.forEach((award) => {
           expect(award.bookId).toBe(num);
@@ -112,13 +194,32 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
   describe('Test response for endpoint /books/:id/details/editions', () => {
     test('It should give correct status code response to GET editions table', (done) => {
-      request(app).get(`/books/${randomInt}}/details/editions`).then((response) => {
+      const num = randomInt;
+      const data = [[
+        {
+          id: 410,
+          isbn10: '1546556714',
+          isbn13: '9656418241510',
+          title: 'out-of-the-box bluetooth',
+          type: 'Hardcover',
+          publisher: 'Klocko Inc',
+          originalPubDate: 'May 22nd 1938',
+          coverurl: 'https://s3.us-east-2.amazonaws.com/hrr37-fec/fec-bookcovers/editionPic0.jpg',
+          bookId: num,
+        },
+      ]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
+      request(app).get(`/books/${num}}/details/editions`).then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
     test('It should give error response when id does not exist', (done) => {
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve([[]]));
       request(app).get(`/books/${randomInt + 100}}/details/editions`).then((response) => {
         expect(response.statusCode).toBe(404);
         done();
@@ -127,6 +228,22 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
     test('It should give back body obj corresponding to right id with correct format', (done) => {
       const num = randomInt;
+      const data = [[
+        {
+          id: 410,
+          isbn10: '1546556714',
+          isbn13: '9656418241510',
+          title: 'out-of-the-box bluetooth',
+          type: 'Hardcover',
+          publisher: 'Klocko Inc',
+          originalPubDate: 'May 22nd 1938',
+          coverurl: 'https://s3.us-east-2.amazonaws.com/hrr37-fec/fec-bookcovers/editionPic0.jpg',
+          bookId: num,
+        },
+      ]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
       request(app).get(`/books/${num}}/details/editions`).then((response) => {
         response.body.forEach((edition) => {
           expect(edition).toEqual(expect.objectContaining({
@@ -150,13 +267,28 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
   describe('Test response for endpoint /books/:id/details/settings', () => {
     test('It should give correct status code response to GET settings table', (done) => {
-      request(app).get(`/books/${randomInt}}/details/settings`).then((response) => {
+      const num = randomInt;
+      const data = [[
+        {
+          id: 228,
+          city: 'East Pierceville',
+          country: 'Eritrea',
+          bookId: num,
+        },
+      ]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
+      request(app).get(`/books/${num}}/details/settings`).then((response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
     test('It should give error response when id does not exist', (done) => {
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve([[]]));
+
       request(app).get(`/books/${randomInt + 100}}/details/settings`).then((response) => {
         expect(response.statusCode).toBe(404);
         done();
@@ -165,6 +297,17 @@ describe('Test endpoint GET /books/:id/details/:table', () => {
 
     test('It should give back body obj corresponding to right id with correct format', (done) => {
       const num = randomInt;
+      const data = [[
+        {
+          id: 228,
+          city: 'East Pierceville',
+          country: 'Eritrea',
+          bookId: num,
+        },
+      ]];
+      mock = jest.spyOn(db, 'getTableData');
+      mock.mockImplementation(() => Promise.resolve(data));
+
       request(app).get(`/books/${num}}/details/settings`).then((response) => {
         response.body.forEach((edition) => {
           expect(edition).toEqual(expect.objectContaining({
